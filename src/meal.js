f54165bc4a3a5b68314371b0927adb39813e6faf
edward krupicka <edwardkrupicka@gmail.com>
2021-08-28 15:27:25 -0600

    Initial commit, warm up.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return "Delicious " + name;
+};
 
-}
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+};
+
+
+function addIngredients(ingredient, array) {
+  if(array.indexOf(ingredient) === -1) {
+    array.push(ingredient);
+  }
+};
+
+
+function formatPrice(price) {
+  return `$${price}`;
+};
+
+
+function decreasePrice (price) {
+  return (9 / 10) * price;
+};
+
+
+function createRecipe (title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
